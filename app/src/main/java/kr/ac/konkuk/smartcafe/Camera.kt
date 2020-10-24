581a94cd76714012801e590a78f84de6d06431b7
jihoahn9303 <48744746+jihoahn9303@users.noreply.github.com>
2020-10-24 19:18:43 +0900

    Update Camera.kt

--- app/src/main/java/kr/ac/konkuk/smartcafe/Camera.kt
+++ app/src/main/java/kr/ac/konkuk/smartcafe/Camera.kt
@@ -3,12 +3,16 @@ package kr.ac.konkuk.smartcafe
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
@@ -25,11 +29,14 @@ class Camera : AppCompatActivity() {
 
     val REQUEST_IMAGE_CAPTURE = 1
     private lateinit var curPhotoPath : String
+    private var mMediaScanner: MediaScanner? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        mMediaScanner = MediaScanner.getInstance(getApplicationContext());
 
         settingPermission() // 권한체크 시작
 
@@ -48,7 +55,7 @@ class Camera : AppCompatActivity() {
             takePictureIntent.resolveActivity(packageManager)?.also {
                 val photoFile: File? = try{
                     createImageFile()
-                }catch(ex: IOException){
+                }catch (ex: IOException){
                     null
                 }
                 photoFile?.also{
@@ -64,9 +71,48 @@ class Camera : AppCompatActivity() {
         }
     }
 
+    private fun rotateImageIfRequired(imagePath: String): Bitmap? {
+        var degrees = 0
+        try {
+            val exif = ExifInterface(imagePath)
+            val orientation = exif.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_NORMAL
+            )
+            when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> degrees = 90
+                ExifInterface.ORIENTATION_ROTATE_180 -> degrees = 180
+                ExifInterface.ORIENTATION_ROTATE_270 -> degrees = 270
+            }
+        } catch (e: IOException) {
+            Log.e("ImageError", "Error in reading Exif data of $imagePath", e)
+        }
+
+        val decodeBounds: BitmapFactory.Options = BitmapFactory.Options()
+        decodeBounds.inJustDecodeBounds = true
+        var bitmap: Bitmap? = BitmapFactory.decodeFile(imagePath, decodeBounds)
+        val numPixels: Int = decodeBounds.outWidth * decodeBounds.outHeight
+        val maxPixels = 2048 * 1536 // requires 12 MB heap 2048 * 1536
+        val options: BitmapFactory.Options = BitmapFactory.Options()
+        options.inSampleSize = if (numPixels > maxPixels) 2 else 1
+        bitmap = BitmapFactory.decodeFile(imagePath, options)
+        if (bitmap == null) {
+            return null
+        }
+        val matrix = Matrix()
+        matrix.setRotate(degrees.toFloat())
+        bitmap = Bitmap.createBitmap(
+            bitmap, 0, 0, bitmap.width,
+            bitmap.height, matrix, true
+        )
+        return bitmap
+
+    }
+
     private fun createImageFile() : File{
         val timeStamp : String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val storageDir : File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
         return File.createTempFile(
             "JPEG_${timeStamp}_",
             ".jpg",
@@ -96,8 +142,9 @@ class Camera : AppCompatActivity() {
             .setDeniedMessage("권한을 거부하셨습니다. [앱 설정] - > [권한] 항목에서 허용해주세요.")
             .setPermissions(
                 android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//                android.Manifest.permission.READ_EXTERNAL_STORAGE,
-                android.Manifest.permission.CAMERA)
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.CAMERA
+            )
             .check()
     }
 
@@ -108,15 +155,17 @@ class Camera : AppCompatActivity() {
             val bitmap: Bitmap
             val file = File(curPhotoPath)
             if (Build.VERSION.SDK_INT < 28) {
+                //iv_profile.setImageBitmap(rotateImageIfRequired(file.path))
+                //rotateImageIfRequired(file.path)
                 bitmap = MediaStore.Images.Media.getBitmap(contentResolver, Uri.fromFile(file))
-                iv_profile.setImageBitmap(bitmap)
             } else {
                 val decode = ImageDecoder.createSource(
                     this.contentResolver,
                     Uri.fromFile(file)
                 )
+                //iv_profile.setImageBitmap(rotateImageIfRequired(file.path))
+                //rotateImageIfRequired(file.path)
                 bitmap = ImageDecoder.decodeBitmap(decode)
-                iv_profile.setImageBitmap(bitmap)
             }
             savePhoto(bitmap)
         }
@@ -130,9 +179,10 @@ class Camera : AppCompatActivity() {
         if(!folder.isDirectory) {
             folder.mkdirs()
         }
+        rotateImageIfRequired(folderPath + "/" + fileName)
         val out = FileOutputStream(folderPath + fileName)
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
-        Toast.makeText(this, "사진이 앨범에 저장되었습니다.",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "사진이 앨범에 저장되었습니다.", Toast.LENGTH_SHORT).show()
+        mMediaScanner?.mediaScanning(folderPath + "/" + fileName);
     }
-
 }
